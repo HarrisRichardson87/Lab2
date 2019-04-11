e86df44056bc1657321f2f8fb5ab61630f19c6e4
Harris <harris.richardson87@gmail.com>
2019-04-11 13:09:55 -0700

    complete

--- js/app.js
+++ js/app.js
@@ -8,34 +8,35 @@ $(document).ready(function() {
     this.image_url = horn.image_url;
     this.title = horn.title;
     this.description = horn.description;
-    this.keywords = horn.keywords;
+    this.keyword = horn.keyword;
     this.horns = horn.horns
     allHorns.push(this);
   }
   Horn.prototype.render = function () {
     const hornSectionHtml = $('#hornAnimal').html();
-   
+
     $('main').append('<section id="clone"></section>');
     $('#clone').html(hornSectionHtml);
-    
     $('#clone').find('h2').text(this.title);
-
+    $('#clone').find('img').attr('id',this.title)
     $('#clone').find('img').attr('src', this.image_url);
     $('#clone').find('alt').attr('alt', this.description);
+    $('#clone').attr('class',this.keyword);
+    $('#clone').attr('id', this.title);
+
     
-    
-    $('#clone').attr('id', '');
-    /////drop down/////
-    const dropSelect = $('#animal').html();
+
     $('#hornAnimalDrop').append('<option id = "drop"></option>');
-    $('#drop').text(this.title);
+    $('#drop').text(this.keyword);
     $('#drop').find('alt').attr('alt',this.description);
-    $('#drop').attr('id', '');
-    
+    $('#drop').attr('id', this.keyword);
+
+
+   
   }
   const testHornedAnimal = new Horn ({});
   testHornedAnimal.render();
-
+  
 
   Horn.collectHorns = () =>{
     $.get('data/page-1.json','json').then(data =>{
@@ -44,12 +45,18 @@ $(document).ready(function() {
     });
   };
   Horn.collectHorns();
-  $("#hornAnimalDrop").change(function() {
-    let $selected = $(this).val();
-    console.log($selected)
-    $('h2:not(:contains("$selected"))').hide();
-    
-  });
   
+
+  $('#hornAnimalDrop').change(function() {
+    /*$(".showAnimals").css('display','none');*/
+    let $selected = $(this).val();
+    console.log($selected);
+    
+    $('section').hide();
+   
+    $(`.${$selected}`).show();
+
+  });
+
 });
 
